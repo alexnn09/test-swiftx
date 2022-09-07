c85d09d463d806ec6fcd6bbb10d52466a1670a8d
alexnn09 <alexnn09@mail.ru>
2022-09-08 02:13:07 +0300

    first commit

---
+++ api.php
@@ -0,0 +1,218 @@
+<?php
+
+new api();
+
+class api
+{
+    private $pdo;
+    private $dsn = 'mysql:host=localhost';
+    private $user = 'user';
+    private $password = 'password';
+    private $dbName = 'db_name';
+    private $requestMethodByMethod = [
+        "staff_info" => "GET",
+        "staff" => "GET",
+        "offices" => "GET",
+        "update_staff" => "POST",
+        "add_staff" => "POST",
+        "delete_staff" => "POST",
+        "search_staff" => "GET"
+    ];
+    private $requiredFields = [
+        "update_staff" => [
+            "id",
+            "fio",
+            "phone",
+            "post",
+            "office_id"
+        ],
+        "add_staff" => [
+            "fio",
+            "phone",
+            "post",
+            "office_id"
+        ],
+        "delete_staff" => [
+            "id"
+        ],
+        "search_staff" => [
+            "fio"
+        ]
+    ];
+    private $fields;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO($this->dsn . ";dbname=" . $this->dbName, $this->user, $this->password);
+        } catch (PDOException $exception) {
+            $this->response([], 500, "Internal error");
+        }
+
+        $this->checkAuth();
+        $this->handle();
+    }
+
+    private function checkAuth()
+    {
+        $login = $_REQUEST['login'];
+        $password = $_REQUEST['passwd'];
+
+        $query = "SELECT id FROM user WHERE login = '" . $login . "' AND passwd = '" . $password . "' LIMIT 1";
+        $result = $this->pdo->query($query);
+
+        if (!$result || $result->rowCount() == 0) {
+            $this->response([], 401, "Authorization Required");
+        } else {
+            $id = $result->fetch()["id"];
+            $this->pdo->query("UPDATE user WHERE id = '" . $id . "' SET last_auth = GETDATE()");
+        }
+    }
+
+    private function handle()
+    {
+        $method = $_REQUEST["method"];
+
+        if ($this->requestMethodByMethod[$method]) {
+            $this->validateRequestMethod($method);
+            $this->fillFields($method);
+            $this->validateRequestData($method);
+            $this->$method();
+        } else {
+            $this->response([], 400, "Method not found");
+        }
+    }
+
+    private function response(array $array = [], int $code = 200, string $message = null)
+    {
+        header('Content-Type: application/json; charset=utf-8');
+        http_response_code($code);
+        echo json_encode(
+            [
+                "status" => $code === 200 ? "ok" : "error",
+                "message" => $message,
+                "data" => $array
+            ]
+        );
+        die;
+    }
+
+    private function validateRequestMethod(string $method)
+    {
+        if ($_SERVER["REQUEST_METHOD"] !== $this->requestMethodByMethod[$method]) {
+            $this->response([], 405, 'Method not allowed');
+        }
+    }
+
+    private function fillFields(string $method)
+    {
+        if ($this->requiredFields[$method]) {
+            foreach ($this->requiredFields[$method] as $fieldKey) {
+                $this->fields[$fieldKey] = $_REQUEST[$fieldKey];
+            }
+        }
+    }
+
+    private function validateRequestData(string $method)
+    {
+        $requiredData = array_flip($this->requiredFields[$method] ?? []);
+
+        if (empty($requiredData)) {
+            return;
+        }
+
+        foreach (array_keys($this->fields) as $rData) {
+            if (isset ($requiredData[$rData])) {
+                unset($requiredData[$rData]);
+            }
+        }
+
+        if (!empty($requiredData)) {
+            $this->response([], 400, 'Required fields not found: ' . implode(", ", array_keys($requiredData)));
+        }
+    }
+
+    private function staff_info()
+    {
+        $query = '
+            SELECT 
+                   s.id as id,
+                   s.fio as fio,
+                   s.post as post,
+                   s.phone as phone,
+                   o.office_name as office_name,
+                   o.address as address 
+            FROM staff as s LEFT JOIN office as o ON s.office = o.id';
+
+        $this->response($this->prepareRows($this->pdo->query($query)));
+    }
+
+    private function staff()
+    {
+        $query = 'SELECT id, fio FROM staff';
+
+        $this->response($this->prepareRows($this->pdo->query($query)));
+    }
+
+    private function offices()
+    {
+        $query = 'SELECT id, office_name, address FROM office';
+
+        $this->response($this->prepareRows($this->pdo->query($query)));
+    }
+
+    private function update_staff()
+    {
+        $query = "
+        UPDATE staff 
+        SET 
+            fio = '" . $this->fields["fio"] . "',
+            phone = '" . $this->fields["phone"] . "',
+            post = '" . $this->fields["post"] . "',
+            office = '" . $this->fields["office_id"]. "'
+        WHERE id='" . $this->fields["id"]."'";
+
+        $this->pdo->exec($query);
+        $this->response([]);
+    }
+
+    private function add_staff()
+    {
+        $query = "
+                INSERT INTO staff (fio, phone, post, office)
+                VALUE (
+                    '" . $this->fields['fio'] . "',
+                    '" . $this->fields["phone"] . "',
+                    '" . $this->fields['post'] . "',
+                    '" . $this->fields["office_id"] . "'
+                    )
+            ";
+
+        $this->pdo->exec($query);
+        $this->response([]);
+    }
+
+    private function delete_staff()
+    {
+        $query = "DELETE FROM staff WHERE id = " . $this->fields["id"];
+
+        $this->pdo->exec($query);
+        $this->response([]);
+    }
+
+    private function search_staff()
+    {
+        $query = "SELECT fio, phone, post FROM staff WHERE fio like '%" . $this->fields["fio"] . "%'";
+
+        $this->response($this->prepareRows($this->pdo->query($query)));
+    }
+
+    private function prepareRows($pdoQuery)
+    {
+        if ($pdoQuery instanceof PDOStatement) {
+            return $pdoQuery->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return [];
+        }
+    }
+}
---
+++ import.php
@@ -0,0 +1,228 @@
+<?php
+
+$data = [
+    [
+        "id" => 1,
+        "fio" => "Иванов Дмитрий Сергеевич",
+        "phone" => 2137,
+        "post" => "Оператор",
+        "office" => "Северный",
+        "address" => "Дзержинского, 20",
+    ],
+    [
+        "id" => 2,
+        "fio" => "Кочкина Дарья Алексеевна",
+        "phone" => 2101,
+        "post" => "Администратор",
+        "office" => "Центральный",
+        "address" => "Ленина, 51",
+    ],
+    [
+        "id" => 3,
+        "fio" => "Сергеев Юрий Витальевич",
+        "phone" => 2112,
+        "post" => "Управляющий",
+        "office" => "Центральный",
+        "address" => "Ленина, 51",
+    ],
+    [
+        "id" => 4,
+        "fio" => "Баранов Сергей Вадимович",
+        "phone" => 2137,
+        "post" => "Специалист",
+        "office" => "Северный",
+        "address" => "Дзержинского, 20",
+    ],
+    [
+        "id" => 5,
+        "fio" => "Михайлов Максим Юрьевич",
+        "phone" => 2122,
+        "post" => "Оператор",
+        "office" => "Западный",
+        "address" => "Солнечная, 37А",
+    ],
+    [
+        "id" => 6,
+        "fio" => "Фомина Анна Валентиновна",
+        "phone" => 2120,
+        "post" => "Кассир",
+        "office" => "Центральный",
+        "address" => "Ленина, 51",
+    ],
+    [
+        "id" => 7,
+        "fio" => "Носков Андрей Витальевич",
+        "phone" => 2131,
+        "post" => "Специалист",
+        "office" => "Октябрьский",
+        "address" => "Октябрьский, 98",
+    ],
+    [
+        "id" => 8,
+        "fio" => "Соколова Анна Валерьевна",
+        "phone" => 2130,
+        "post" => "Бухгалтер",
+        "office" => "Центральный",
+        "address" => "Ленина, 51",
+    ],
+    [
+        "id" => 9,
+        "fio" => "Шахматов Алексей Алексеевич",
+        "phone" => 2125,
+        "post" => "Специалист",
+        "office" => "Западный",
+        "address" => "Солнечная, 37А",
+    ],
+    [
+        "id" => 10,
+        "fio" => "Ершов Дмитрий Иванович",
+        "phone" => 2137,
+        "post" => "Специалист",
+        "office" => "Октябрьский",
+        "address" => "Октябрьский, 98",
+    ],
+];
+
+$users = [
+    [
+        "id" => 1,
+        "login" => "admin",
+        "passwd" => "test123"
+    ],
+    [
+        "id" => 2,
+        "login" => "manager",
+        "passwd" => "qweasd"
+    ]
+];
+
+$import = new importData($data, $users);
+$import->importData();
+
+class importData
+{
+    private $dsn = 'mysql:host=localhost';
+    private $user = 'user';
+    private $password = 'password';
+    private $dbName = 'db_name';
+    private $pdo;
+    private $data;
+    private $staff;
+    private $offices;
+    private $officeIdByHash;
+    private $users;
+
+    public function __construct(
+        array $data,
+        array $users
+    )
+    {
+        $this->data = $data;
+        $this->users = $users;
+        $pdo = new PDO($this->dsn, $this->user, $this->password);
+        $pdo->exec('CREATE DATABASE '.$this->dbName);
+        $this->pdo = new PDO($this->dsn . ";dbname=" . $this->dbName, $this->user, $this->password);
+
+        $this->createTables();
+        $this->prepareData();
+    }
+
+    private function createTables()
+    {
+        $this->pdo->exec('
+                CREATE TABLE staff (
+                    id INT UNSIGNED AUTO_INCREMENT primary key NOT NULL,
+                      fio VARCHAR(255) NOT NULL,
+                       phone SMALLINT UNSIGNED NOT NULL,
+                        post VARCHAR(255) DEFAULT NULL,
+                         office INT UNSIGNED NOT NULL)
+                         ');
+
+        $this->pdo->query('
+                CREATE TABLE office (
+                    id INT UNSIGNED AUTO_INCREMENT primary key NOT NULL,
+                     hash VARCHAR(32) NOT NULL,
+                      office_name VARCHAR(255) NOT NULL,
+                       address VARCHAR(255) NOT NULL)
+                       ');
+
+        $this->pdo->query('
+                CREATE TABLE user (
+                    id INT UNSIGNED AUTO_INCREMENT primary key NOT NULL,
+                    login VARCHAR(255) NOT NULL,
+                    passwd VARCHAR(255) NOT NULL,
+                    last_auth DATETIME DEFAULT NULL
+                )');
+    }
+
+    private function prepareData()
+    {
+        foreach ($this->data as $item) {
+            $officeHash = md5($item['office'] . $item['address']);
+
+            $this->staff[] = [
+                'id' => $item['id'],
+                'fio' => $item['fio'],
+                'phone' => $item['phone'],
+                'post' => $item['post'],
+                'officeHash' => $officeHash
+            ];
+
+            if (!$this->offices[$officeHash]) {
+                $this->offices[$officeHash] = [
+                    'hash' => $officeHash,
+                    'office_name' => $item['office'],
+                    'address' => $item['address']
+                ];
+            }
+        }
+    }
+
+    public function importData()
+    {
+        $this->importOffices();
+        $this->importStaff();
+        $this->importUsers();
+    }
+
+    private function importOffices()
+    {
+        foreach ($this->offices as $office) {
+            $this->pdo->query("
+                INSERT INTO office (hash, office_name, address)
+                VALUE ('" . $office['hash'] . "', '" . $office['office_name'] . "', '" . $office["address"] . "');
+            ");
+            $this->officeIdByHash[$office["hash"]] = $this->pdo->lastInsertId();
+        }
+    }
+
+    private function importStaff()
+    {
+        foreach ($this->staff as $staff) {
+            $this->pdo->query("
+                INSERT INTO staff (id, fio, phone, post, office)
+                VALUE (
+                    '" . $staff['id'] . "',
+                    '" . $staff['fio'] . "',
+                    '" . $staff["phone"] . "',
+                    '" . $staff['post'] . "',
+                    '" . $this->officeIdByHash[$staff["officeHash"]] . "'
+                    )
+            ");
+        }
+    }
+
+    private function importUsers()
+    {
+        foreach ($this->users as $user) {
+            $this->pdo->query("
+                INSERT INTO user (id, login, passwd)
+                VALUE (
+                    '" . $user['id'] . "',
+                    '" . $user['login'] . "',
+                    '" . $user["passwd"] . "'
+                    )
+            ");
+        }
+    }
+}
